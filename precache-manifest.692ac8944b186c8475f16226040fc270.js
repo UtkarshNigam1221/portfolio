b491c1d336e44bf5ee27a1944f689d026a7d0d21
Kuro Gaming <utkarshnigam1221@gmail.com>
2025-05-28 21:58:28 +0530

    Updates

---
+++ precache-manifest.692ac8944b186c8475f16226040fc270.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d650e293b236332b02846665f72d67fb",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5fda57a5674ebf7e14f6",
+    "url": "/portfolio/static/css/2.58736d6f.chunk.css"
+  },
+  {
+    "revision": "ac2b5ab14432561ec353",
+    "url": "/portfolio/static/css/main.9893f67a.chunk.css"
+  },
+  {
+    "revision": "5fda57a5674ebf7e14f6",
+    "url": "/portfolio/static/js/2.c758d6d0.chunk.js"
+  },
+  {
+    "revision": "46a379a572333860f78765c743603e43",
+    "url": "/portfolio/static/js/2.c758d6d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac2b5ab14432561ec353",
+    "url": "/portfolio/static/js/main.c8ca0f89.chunk.js"
+  },
+  {
+    "revision": "0d4bf47b70941af9967b",
+    "url": "/portfolio/static/js/runtime-main.8940859c.js"
+  },
+  {
+    "revision": "8539de93671a3fa58efc05a680a82b90",
+    "url": "/portfolio/static/media/profile.8539de93.jpeg"
+  }
+]);
