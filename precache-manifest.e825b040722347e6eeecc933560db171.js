faca81233b1b6427d9788cc67b8899b42c96e2be
Kuro Gaming <utkarshnigam1221@gmail.com>
2025-05-29 12:20:16 +0530

    Updates

---
+++ precache-manifest.e825b040722347e6eeecc933560db171.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4b05c3ac167c990c5a80d8ad421ad3c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5fda57a5674ebf7e14f6",
+    "url": "/portfolio/static/css/2.58736d6f.chunk.css"
+  },
+  {
+    "revision": "1138725a10f27da95bbc",
+    "url": "/portfolio/static/css/main.9893f67a.chunk.css"
+  },
+  {
+    "revision": "5fda57a5674ebf7e14f6",
+    "url": "/portfolio/static/js/2.c758d6d0.chunk.js"
+  },
+  {
+    "revision": "46a379a572333860f78765c743603e43",
+    "url": "/portfolio/static/js/2.c758d6d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1138725a10f27da95bbc",
+    "url": "/portfolio/static/js/main.a7cc31c2.chunk.js"
+  },
+  {
+    "revision": "0d4bf47b70941af9967b",
+    "url": "/portfolio/static/js/runtime-main.8940859c.js"
+  },
+  {
+    "revision": "8539de93671a3fa58efc05a680a82b90",
+    "url": "/portfolio/static/media/profile.8539de93.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-frontend-dev-portfolio/precache-manifest.9cb59fef87ed1329d06165b7cadb1bf8.js"
+  "/portfolio/precache-manifest.e825b040722347e6eeecc933560db171.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-frontend-dev-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
